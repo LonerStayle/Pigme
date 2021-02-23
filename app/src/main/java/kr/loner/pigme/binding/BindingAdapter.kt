af41e825afb73c57b19e79e20a90fc939184b944
Loner123 <dlwlstjq410@gmail.com>
2021-02-23 20:44:04 +0900

    [Modify] 사진 안뜨는 문제 해결

--- app/src/main/java/kr/loner/pigme/binding/BindingAdapter.kt
+++ app/src/main/java/kr/loner/pigme/binding/BindingAdapter.kt
@@ -4,5 +4,5 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-@BindingAdapter("imageUrl")
-fun bindingAdapter(view: ImageView,url:String) = Glide.with(view.context).load(url).into(view)
+@BindingAdapter("app:image")
+fun bindingAdapter(view: ImageView,url:String) = Glide.with(view.context).load(url).centerCrop().into(view)
--- app/src/main/java/kr/loner/pigme/view/imageurl/ImageUrl.kt
+++ app/src/main/java/kr/loner/pigme/view/imageurl/ImageUrl.kt
@@ -1,7 +1,7 @@
 package kr.loner.pigme.view.imageurl
 
  fun imageUrl(image: String): String {
-    var uriStringValue = "android.resource://com.example.pigme/$image"
+    var uriStringValue = "android.resource://kr.loner.pigme/$image"
     if (image.length > 20) {
         uriStringValue = image
     }
